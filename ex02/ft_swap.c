62875cf9ae0c2ada6fde16cd3041d97fda66e810
Tiago Pinho <60488719+cYDOGBI@users.noreply.github.com>
2025-07-21 16:59:41 +0100

    Add files via upload

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -1,23 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlaranje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/21 16:06:39 by tlaranje          #+#    #+#             */
-/*   Updated: 2025/07/21 16:06:44 by tlaranje         ###   ########.fr       */
+/*   Created: 2025/07/21 16:40:28 by tlaranje          #+#    #+#             */
+/*   Updated: 2025/07/21 16:40:31 by tlaranje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
 
-void ft_ft(int *nbr) {
-    *nbr = 42;
+void ft_swap(int *a, int *b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
 
 // int main(void) {
-//     int x = 0;
-//     ft_ft(&x);
-//     printf("x is now: %d\n", x);
+//     int a = 1;
+//     int b = 2;
+
+//     printf("Before A: %d, Before B: %d\n", a, b);
+//     ft_swap(&a, &b);
+//     printf("After A: %d, After B: %d\n", a, b);
 //     return 0;
 // }
